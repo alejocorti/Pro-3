1f543f0a3a29c509e26d12b287ce93ab95d3819f
mariavaras <mariavarascastex@icloud.com>
2023-09-12 20:15:40 -0300

    terminando agregar y sacar favoritos

--- src/paginas/detalle.js
+++ src/paginas/detalle.js
@@ -8,13 +8,13 @@ class Detalle extends Component {
         super(props)
         this.state = {
             cargando: true,
-            favorito: false,        
+            favorito: false,
             pelicula: null
         }
     }
     componentDidMount() {
         const id = this.props.match.params.id;
-        const url = 'https://api.themoviedb.org/3/movie/'+id+'language=en-US'
+        const url = 'https://api.themoviedb.org/3/movie/' + id + 'language=en-US'
 
         console.log(id)
         const opciones = {
@@ -27,66 +27,83 @@ class Detalle extends Component {
         fetch(url, opciones)
             .then(data => data.json())
             .then(data => {
-                this.setState({ cargando: false, favorito: false, pelicula: data })
+                let storageFav = localStorage.getItem('favoritos')
+                let arrParseado = JSON.parse(storageFav)
+                let estaMiPelicula = false
+                if (arrParseado !== null) {
+                 estaMiPelicula = arrParseado.includes(data.id)
+
+                }
+                this.setState({ cargando: false, favorito: estaMiPelicula, pelicula: data })
 
             })
 
     }
 
-    agregarAFavoritos(id){
+    agregarAFavoritos(id) {
         let storageFav = localStorage.getItem('favoritos')
-        if(storageFav === null){
-          let arrIds = [id]
-          let arrStringificado = JSON.stringify(arrIds)
-          localStorage.setItem('favoritos', arrStringificado)
+        if (storageFav === null) {
+            let arrIds = [id]
+            let arrStringificado = JSON.stringify(arrIds)
+            localStorage.setItem('favoritos', arrStringificado)
         } else {
-          let arrParseado = JSON.parse(storageFav)
-          arrParseado.push(id)
-          let arrStringificado = JSON.stringify(arrParseado)
-          localStorage.setItem('favoritos', arrStringificado)
+            let array = JSON.parse(storageFav)
+            array.push(id)
+            let arrStringificado = JSON.stringify(array)
+            localStorage.setItem('favoritos', arrStringificado)
         }
-  
+
         this.setState({
-          favorito: true,
-          pelicula: this.state.pelicula,
-          cargando: this.state.cargando, 
+            favorito: true,
+            pelicula: this.state.pelicula,
+            cargando: this.state.cargando,
         })
     }
-    sacarDeFavoritos(id){
-
+    sacarDeFavoritos(id) {
+        let storage = localStorage.getItem('favoritos')
+        let arrParseado = JSON.parse(storage)
+        let favsFiltrados = arrParseado.filter((idPelicula) => idPelicula !== id)
+        let arrStringificado = JSON.stringify(favsFiltrados)
+        localStorage.setItem('favoritos', arrStringificado)
+        
+        this.setState({
+            favorito: false,
+            pelicula: this.state.pelicula,
+            cargando: this.state.cargando,
+        })
     }
 
     render() {
-        if (this.state.cargando){
-            return( <Loader></Loader>)
+        if (this.state.cargando) {
+            return (<Loader></Loader>)
         }
         return (
             <div>
-                   <h1 className="titulo" >Detalle</h1>
+                <h1 className="titulo" >Detalle</h1>
                 <main>
                     <section className="top-data">
                         <div id="detalle">
                             <div>
-                                <img src={"https://image.tmdb.org/t/p/w342/" +this.state.pelicula.backdrop_path}/>
+                                <img src={"https://image.tmdb.org/t/p/w342/" + this.state.pelicula.backdrop_path} />
                             </div>
                             <div>
                                 <h2>{this.state.pelicula.title}</h2>
                                 <p>Calificacion: {this.state.pelicula.vote_average}</p>
                                 <p>Estreno: {this.state.pelicula.release_date}</p>
                                 <p>Overview: {this.state.pelicula.overview}</p>
-                                <p>Genero: {this.state.pelicula.genres.map(g=><span key ={g.id}>{g.name} </span>)}</p>
+                                <p>Genero: {this.state.pelicula.genres.map(g => <span key={g.id}>{g.name} </span>)}</p>
                                 <p>Duracion: {this.state.pelicula.runtime}min</p>
-                                <p> 
-                                {
-                                    this.state.favorito ?
-                                    <button onClick={()=> this.sacarDeFavoritos(this.state.pelicula.id)}>
-                                        Sacar de favoritos
-                                    </button>  
-                                    :
-                                    <button onClick={()=> this.agregarAFavoritos(this.state.pelicula.id)}>
-                                        Agregar a favoritos
+                                <p>
+                                    {
+                                        this.state.favorito ?
+                                            <button onClick={() => this.sacarDeFavoritos(this.state.pelicula.id)}>
+                                                Sacar de favoritos
                                     </button>
-                                }
+                                            :
+                                            <button onClick={() => this.agregarAFavoritos(this.state.pelicula.id)}>
+                                                Agregar a favoritos
+                                    </button>
+                                    }
                                 </p>
                             </div>
 
